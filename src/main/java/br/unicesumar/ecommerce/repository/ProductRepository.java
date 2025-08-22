6957c6285b09ccd2759961f3afe9c23df7f1f3f1
eder <eder.bastos21@gmail.com>
2025-08-22 10:49:41 -0300

    [feature] add price and ammount sold search in product

--- src/main/java/br/unicesumar/ecommerce/repository/ProductRepository.java
+++ src/main/java/br/unicesumar/ecommerce/repository/ProductRepository.java
@@ -4,9 +4,12 @@ import br.unicesumar.ecommerce.model.Product;
 import br.unicesumar.ecommerce.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByNameContainingIgnoreCase(String name);
+    List<Product> findByPrice(BigDecimal price);
+    List<Product> findByAmmountSold(int ammountSold);
 }
--- src/main/java/br/unicesumar/ecommerce/service/ProductService.java
+++ src/main/java/br/unicesumar/ecommerce/service/ProductService.java
@@ -2,6 +2,8 @@ package br.unicesumar.ecommerce.service;
 import br.unicesumar.ecommerce.model.Product;
 import br.unicesumar.ecommerce.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
@@ -19,6 +21,15 @@ public class ProductService {
     public List<Product> findByNameContainingIgnoreCase(String name){
         return productRepository.findByNameContainingIgnoreCase(name);
     }
+
+    public List<Product> findByPrice (BigDecimal price){
+        return productRepository.findByPrice(price);
+    }
+
+    public List<Product> findByAmmountSold (int ammountSold){
+        return productRepository.findByAmmountSold(ammountSold);
+    }
+
     public Product save(Product product) {
         return productRepository.save(product);
     }
